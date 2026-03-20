994f3100d1cc9d5323be7b1f500e967d7a35a3a1
nadeeshancloud-lang <nadeeshancloud@gmail.com>
2026-03-20 21:39:12 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || 'NIMMA_SESSION_ID_ILLI_HAKI',
+    BOT_NAME: 'KANISHKA-MDX',
+    OWNER_NAME: 'Kanishka',
+    PREFIX: '.',
+    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/kanishka-mdx'
+};
